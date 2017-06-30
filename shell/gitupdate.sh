4844f979f0932edcbb1cd21090153e4806a489ee
Vasiley <hwindecker@yahoo.com>
2017-06-30 06:23:47 -0500

    Changes to be committed: 	modified:   Sensors.yaml 	modified:   packages/wink_fan.yaml 	modified:   shell/gitupdate.sh

--- shell/gitupdate.sh
+++ shell/gitupdate.sh
@@ -1,14 +1,14 @@
-#!/bin/bash
+
 
 cd /home/homeassistant/.homeassistant
 source /srv/homeassistant/bin/activate
-hass --script check_config
 
-git add .
-git status
+
+sudo git add .
+sudo git status
 echo -n "Enter the Description for the Change: " [Minor Update]
 read CHANGE_MSG
-git commit -m "${CHANGE_MSG}"
-git push origin master
+sudo git commit -m "${CHANGE_MSG}"
+sudo git push origin master
 
 exit
